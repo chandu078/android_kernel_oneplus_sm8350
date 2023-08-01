b7d239f1ab127e61eaca054fcfac88db05de5295
chandu078 <chandudyavanapelli03@gmail.com>
2023-08-01 04:11:54 +0200

    [SQUASH]: techpack/display: Avoid enforcing ulps suspend
    This reverts commit 9f96de05a95db612baa2c516c0c0a6d0a3d6f4d3.
    
    Revert "techpack/display: Enforce ulps by default"
    
    This reverts commit 1e3f2ee22003583bd413f81b5860c0a1fffef494.
    
    Reason for revert: It's breaks our MEMC feature.

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -2936,12 +2936,14 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 	const char *string;
 	int i, rc = 0;
 
-	panel->ulps_feature_enabled = true;
+	panel->ulps_feature_enabled =
+		utils->read_bool(utils->data, "qcom,ulps-enabled");
 
 	DSI_DEBUG("%s: ulps feature %s\n", __func__,
 		(panel->ulps_feature_enabled ? "enabled" : "disabled"));
 
-	panel->ulps_suspend_enabled = true;
+	panel->ulps_suspend_enabled =
+		utils->read_bool(utils->data, "qcom,suspend-ulps-enabled");
 
 	DSI_DEBUG("%s: ulps during suspend feature %s\n", __func__,
 		(panel->ulps_suspend_enabled ? "enabled" : "disabled"));
